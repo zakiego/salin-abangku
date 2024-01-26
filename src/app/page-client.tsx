0db065fbd0ac3fee6510bead57d9164a27748bd8
softone-as <shofwanas12@gmail.com>
2024-01-26 23:59:23 +0700

    feat (translation): move shuffle into client page bangdaa 🙇‍♂️

--- src/app/page-client.tsx
+++ src/app/page-client.tsx
@@ -17,7 +17,6 @@ import { useClipboard } from "@mantine/hooks";
 import { useState } from "react";
 import { FiClipboard } from "react-icons/fi";
 import { Toaster, toast } from "sonner";
-import { shuffle } from "./page";
 
 const successToastID = [
   "Tercopy abangkuuuhh 🔥🔥",
@@ -178,3 +177,23 @@ export default function PageClient({
     </Container>
   );
 }
+
+const shuffle = (array: string[]) => {
+  let currentIndex = array.length;
+  let randomIndex: number;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
