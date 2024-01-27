03cef299d75681a435b844de388b2ac94f739c3d
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-01-27 22:13:37 +0800

    Update API routes to support en language

--- src/app/api/one/route.ts
+++ src/app/api/one/route.ts
@@ -1,9 +1,18 @@
-import { templates } from "@/lib/template";
-import { NextResponse } from "next/server";
+import { templates, templatesEn } from "@/lib/template";
+import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
+	const lang = request.nextUrl.searchParams.get("lang");
+
+	if (lang === "en") {
+		const random = Math.floor(Math.random() * templatesEn.length);
+		const template = templatesEn[random];
+
+		return NextResponse.json(template);
+	}
+
 	const random = Math.floor(Math.random() * templates.length);
 	const template = templates[random];
 
