c4793ad1c8a64218e6af37520d4aaca61a6a4423
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-01-29 23:03:55 +0800

    Add CORS headers for API

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,17 +1,23 @@
+import { corsHeaders } from "@/app/api/cors";
 import { templates } from "@/lib/template";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-static"; // defaults to auto
 
 export async function GET(request: Request) {
-	return NextResponse.json({
-		one: {
-			id: "https://salin-abangku.vercel.app/api/one",
-			en: "https://salin-abangku.vercel.app/api/one?lang=en",
+	return NextResponse.json(
+		{
+			one: {
+				id: "https://salin-abangku.vercel.app/api/one",
+				en: "https://salin-abangku.vercel.app/api/one?lang=en",
+			},
+			all: {
+				id: "https://salin-abangku.vercel.app/api/all",
+				en: "https://salin-abangku.vercel.app/api/all?lang=en",
+			},
 		},
-		all: {
-			id: "https://salin-abangku.vercel.app/api/all",
-			en: "https://salin-abangku.vercel.app/api/all?lang=en",
+		{
+			headers: corsHeaders,
 		},
-	});
+	);
 }
