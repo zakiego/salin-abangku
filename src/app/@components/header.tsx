24c7a78753467b424e2c04eb90d437f7de3e0268
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-06 23:17:15 +0800

    Add navbar

--- src/app/@components/header.tsx
+++ src/app/@components/header.tsx
@@ -6,59 +6,31 @@ import { MantineLogo } from "@mantinex/mantine-logo";
 import classes from "./header.module.css";
 import { BiChevronDown } from "react-icons/bi";
 import Link from "next/link";
-
-const links = [
-	{ link: "/about", label: "Features" },
-	{
-		link: "#1",
-		label: "Learn",
-		links: [
-			{ link: "/docs", label: "Documentation" },
-			{ link: "/resources", label: "Resources" },
-			{ link: "/community", label: "Community" },
-			{ link: "/blog", label: "Blog" },
-		],
-	},
-	{ link: "/about", label: "About" },
-	{ link: "/pricing", label: "Pricing" },
-	{
-		link: "#2",
-		label: "Support",
-		links: [
-			{ link: "/faq", label: "FAQ" },
-			{ link: "/demo", label: "Book a demo" },
-			{ link: "/forums", label: "Forums" },
-		],
-	},
-];
+import { ActionIcon } from "@mantine/core";
+import Image from "next/image";
 
 export function Header() {
-	const [opened, { toggle }] = useDisclosure(false);
-
-	const items = links.map((link) => {
-		return (
-			<Link key={link.label} href={link.link} className={classes.link}>
-				{link.label}
-			</Link>
-		);
-	});
-
 	return (
 		<header className={classes.header}>
-			<Container size="md">
+			<Container size="xs">
 				<div className={classes.inner}>
 					<Title
-						size="h5"
+						size="h4"
 						style={{
 							textAlign: "center",
 						}}
 					>
 						Salin Abangkuuuhh 🫡🔥🔝
 					</Title>
-					<Group gap={5} visibleFrom="sm">
-						{items}
-					</Group>
-					<Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+
+					<ActionIcon
+						variant="subtle"
+						color="gray"
+						component="a"
+						href="https://github.com/zakiego/salin-abangku"
+					>
+						<Image src="/github.svg" alt="GitHub" width={16} height={16} />
+					</ActionIcon>
 				</div>
 			</Container>
 		</header>
