c9b79e07a7048cf2277cce20f8d6a6d6e7c31d45
bryanltobing <bryantobing0@gmail.com>
2024-03-09 16:24:06 +0800

    fix: simplify logic to prevent missing cors headers in return response

--- src/app/api/one/route.ts
+++ src/app/api/one/route.ts
@@ -1,5 +1,5 @@
 import { corsHeaders } from "@/app/api/cors";
-import { templates, templatesEn } from "@/lib/template";
+import { templates as templatesId, templatesEn } from "@/lib/template";
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // defaults to auto
@@ -7,13 +7,7 @@ export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET(request: NextRequest) {
   const lang = request.nextUrl.searchParams.get("lang");
 
-  if (lang === "en") {
-    const random = Math.floor(Math.random() * templatesEn.length);
-    const template = templatesEn[random];
-
-    return NextResponse.json(template);
-  }
-
+  const templates = lang === "en" ? templatesEn : templatesId;
   const random = Math.floor(Math.random() * templates.length);
   const template = templates[random];
 
